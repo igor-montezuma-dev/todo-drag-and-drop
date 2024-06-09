6b6738b6cc742a045cf67870fddda6955847606c
IgorMontezuma20 <igorminranda17@gmail.com>
2024-06-09 01:28:21 -0300

    feat: Adiocionado o método para criar atividade e implementando o drag and drop

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,21 +1,52 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ITask } from 'src/app/model/task';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todoForm!: FormGroup;
+  tasks: ITask[] = [];
+  inProgress: ITask[] = [];
+  done: ITask[] = [];
 
-  todoForm !: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.todoForm = this.fb.group({
-      activity_name: ['', [Validators.required]]
+      activity_name: ['', [Validators.required]],
     });
   }
 
+  addTask() {
+    this.tasks.push({
+      description: this.todoForm.value.activity_name,
+      done: false,
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
